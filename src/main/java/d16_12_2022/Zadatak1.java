259accec431e1c74498acb62076bf419165ececa
Miljan Novicevic <novicevic.miljan@gmail.com>
2022-12-18 14:43:36 +0100

    Domaci 16.12.2022

---
+++ src/main/java/d16_12_2022/Zadatak1.java
@@ -0,0 +1,17 @@
+package d16_12_2022;
+
+import java.util.Scanner;
+
+public class Zadatak1 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("unesite vrednost za x: ");
+        int x = s.nextInt();
+        int y = 1;
+        for (int i = 1; i < 10; i++) {
+            System.out.println(x +" x " + y++);
+
+        }
+    }
+}
---
+++ src/main/java/d16_12_2022/Zadatak2.java
@@ -0,0 +1,13 @@
+package d16_12_2022;
+
+public class Zadatak2 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 25; i++) {
+            if(i % 5 == 0){
+                System.out.println();           }
+            System.out.print("_ ");
+
+        }
+    }
+}
